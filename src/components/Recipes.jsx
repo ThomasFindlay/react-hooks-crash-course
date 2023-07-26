d52f8f17961f496a38a951b0eeb6ffa52a62b377
Thomas Findlay <thomasfindlay94@gmail.com>
2023-07-26 14:04:50 +0100

    Removed code

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,10 +1,5 @@
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/search.php";
 
-const initialState = {
-  query: "",
-  recipes: [],
-};
-
 const Recipes = props => {
   return (
     <div className="container mx-auto">
