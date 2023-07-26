e538e4a08d10d55cead50214bf491e2686c77d0b
Thomas Findlay <thomasfindlay94@gmail.com>
2023-07-26 14:03:28 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import RecipeModal from "./components/RecipeModal";
 import Recipes from "./components/Recipes";
-import { RecipeContextProvider } from "./components/context/Recipe.context";
 
 function App() {
   return (
     <>
       <div>
-        <RecipeContextProvider>
-          <Recipes />
-          <RecipeModal />
-        </RecipeContextProvider>
+        <Recipes />
       </div>
     </>
   );
